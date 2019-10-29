98d44e72670f3a1d331c420e8cc1de02d8379b24
Thiago Rafael Casagrande <thiagorcasagrande@gmail.com>
2019-10-29 20:13:32 -0300

    Testes de matrícula

--- app/models/aluno_matriculado.rb
+++ app/models/aluno_matriculado.rb
@@ -2,6 +2,7 @@ class AlunoMatriculado < ApplicationRecord
   belongs_to :disciplina
   belongs_to :aluno, class_name: 'Pessoa'
 
+  validates :ano, presence: true
   validates :aluno, uniqueness: { scope: [:disciplina_id, :ano],
                                   message: 'Aluno já está matriculado nesta disciplina para este ano' }
 
--- spec/models/aluno_matriculado_spec.rb
+++ spec/models/aluno_matriculado_spec.rb
@@ -2,39 +2,59 @@ require 'rails_helper'
 
 RSpec.describe AlunoMatriculado, type: :model do
 
-  # context 'validações gerais' do
-  #   # it { should validate_uniqueness_of(:aluno).scoped_to(:disciplina_id, :ano) }
-  # end
+  context 'validações gerais' do
+    it { should belong_to(:disciplina) } 
+    it { should belong_to(:aluno).class_name('Pessoa') } 
+    it { should validate_presence_of(:ano) }
+  end
 
-  # context 'ao matricular o aluno ' do
-  #   let(:coordenador) { Pessoa.new(nome: 'Fábio Taffe', perfil: :coordenador) }
-  #   let(:aluno1) { Pessoa.new(nome: 'Joaquim Teixeira', perfil: :aluno) }
-  #   let(:curso) { Curso.new(nome: 'Sistemas de Informação', coordenador: coordenador) }
-  #   let(:disciplina) { Disciplina.new(curso: curso, nome: 'Engenharia de Software III', semestre: 4) }
-  #   let(:matricula) { AlunoMatriculado.new(ano: 2019, disciplina: disciplina, aluno: aluno1) }
+  context 'ao matricular o aluno ' do
+    let(:aluno) { Pessoa.new(perfil: :aluno, 
+                             nome: 'Tião Macalé') }
+    let(:professor) { Pessoa.new(perfil: :professor, 
+                                 nome: 'Joaquim Teixeira') }
+    let(:coordenador) { Pessoa.new(perfil: :coordenador, 
+                                   nome: 'Fábio Taffe') }
 
-  #   context 'duas vezes na mesma disciplina' do
-  #     it 'e no mesmo ano, deve bloquear' do
-  #       matricula.save
-  #       matricula_repetida = AlunoMatriculado.new(ano: 2019, disciplina: disciplina, aluno: aluno1)
-  #       expect(matricula_repetida).to_not be_valid
-  #     end
+    let(:curso) { Curso.new(nome: 'Sistemas de Informação', 
+                            coordenador: coordenador) }
+    let(:disciplina) { Disciplina.new(curso: curso, 
+                                      nome: 'Eng. SW III',
+                                      professor: professor, 
+                                      semestre: 4) }
+    let(:matricula) { AlunoMatriculado.new(ano: 2019, 
+                                           disciplina: disciplina, 
+                                           aluno: aluno) }
 
-  #     it 'e em anos diferentes, deve permitir' do
-  #       matricula.save
-  #       outra_matricula = AlunoMatriculado.new(ano: 2020, disciplina: disciplina, aluno: aluno1)
-  #       expect(outra_matricula).to be_valid
-  #     end
-  #   end
+    context 'duas vezes na mesma disciplina' do
+      let(:matricula_repetida) { AlunoMatriculado.new(ano: 2019, 
+                                                      disciplina: disciplina, 
+                                                      aluno: aluno) }
 
-  #   context 'em duas disciplinas distintas no mesmo ano' do
-  #     let(:disciplina2) { Disciplina.new(curso: curso, nome: 'Design de Interação', semestre: 4) }
+      it 'e no mesmo ano, deve bloquear' do
+        matricula.save
+        expect(matricula_repetida).to_not be_valid
+      end
 
-  #     it 'deve permitir' do
-  #       matricula.save
-  #       outra_matricula = AlunoMatriculado.new(ano: 2019, disciplina: disciplina2, aluno: aluno1)
-  #       expect(outra_matricula).to be_valid
-  #     end
-  #   end
-  # end
+      it 'e em anos diferentes, deve permitir' do
+        matricula.save
+        matricula_repetida.ano = 2020
+        expect(matricula_repetida).to be_valid
+      end
+    end
+
+    context 'em duas disciplinas distintas no mesmo ano' do
+      let(:disciplina2) { Disciplina.new(curso: curso, 
+                                         nome: 'Design de Interação',
+                                         professor: professor, 
+                                         semestre: 5) }
+      let(:matricula2) { AlunoMatriculado.new(ano: 2019, 
+                                              disciplina: disciplina2, 
+                                              aluno: aluno) }
+      it 'deve permitir' do
+        matricula.save
+        expect(matricula2).to be_valid
+      end
+    end
+  end
 end
